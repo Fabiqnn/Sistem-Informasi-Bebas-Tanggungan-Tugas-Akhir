6a67346ac9faf007897115a4153ad697ade156c2
Fabiqnn <arema20056@gmail.com>
2024-11-14 08:40:03 +0700

    Login

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styleLogin.css" type="text/css">
     <title>Login</title>
 </head>
@@ -27,10 +29,12 @@
                     </div>
                     <button type="submit">Masuk</button>
                 </form>
+                <a href="">Lupa Sandi? Klik disini</a>
             </div>
         </div>
     
         <div class="informasi">
+            <img src="img/Untitled design (8).png" alt="">
             <h1>Selamat Datang</h1>
             <p>Mempersembahkan platform resmi Jurusan Teknologi Informasi untuk mengakses berbagai dokumen akademik dan penelitian jurusan.</p>
         </div>
