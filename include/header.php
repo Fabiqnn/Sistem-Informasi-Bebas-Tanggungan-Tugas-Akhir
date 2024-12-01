003c06cda110e68f538af267a85aee03ef3b35c0
Fabiqnn <arema20056@gmail.com>
2024-12-01 13:05:15 +0700

    header +

--- include/header.php
+++ include/header.php
@@ -1,6 +1,9 @@
 <link rel="stylesheet" href="../assets/css/style-header.css">
 <header>
-    <h1>SIBETA</h1>
+    <h1>
+        <img src="../assets/images/logo-pbl.png" alt="logo">
+        SIBETA
+    </h1>
     <div class="username"> 
     </div>
 </header>
